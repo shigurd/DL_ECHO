fe4647991971cb3e36ff07d10a891572f4afcfbb
Sigurd <sigzha@gmail.com>
2021-08-24 22:54:35 +0200

    combined elongated gaussian/dot mask creation with coordinate extraction into one process. also fixed rounding error bug with true cm coordinates getting logged into the keyfile instead of rounded true cm coordinates based of the rounded true pix coordinates calculated with the 256 * 256 size bottleneck

--- LVOT_point_detection/extract_coords_from_dicom_LVOT.py
+++ LVOT_point_detection/extract_coords_from_dicom_LVOT.py
@@ -1,4 +1,5 @@
 """This scripts generate Masks from AFIRoi Images """
+import os
 import os.path as path
 import sys
 import csv
@@ -10,9 +11,24 @@ if __name__ == "__main__":
     ''' extract LVOT images and LVOT coordinates and LVOTd data into a keyfile '''
     keyfile_name = 'keyfile_GE1424.csv'
 
+    '''define input dir and parameters for mask '''
     input_dir = r'H:\ML_LVOT\dcm_lvot_backup\GE1424_lvot_dcm'
-    output_imgs_dir = path.join('datasets', 'imgs', 'GE1424_HMLHML')
+    height = 256
+    width = 256
+    with_gaussian = True
+    x_warp = 5
+    y_warp = 1
+
+    if with_gaussian == True:
+        dataset_name = f'GE1424X{x_warp}_HMLHML'
+    else:
+        dataset_name = f'GE1424_HMLHML'
+
     keyfile_output = path.join(r'H:\ML_LVOT\txt_keyfile_and_duplicate', keyfile_name)
+    output_imgs_dir = path.join('datasets', 'imgs', dataset_name)
+    output_masks_dir = path.join('datasets', 'masks', dataset_name)
+    os.mkdir(output_imgs_dir)
+    os.mkdir(output_masks_dir)
 
     keyfile_log = open(keyfile_output, 'w', newline='')
     writer = csv.writer(keyfile_log)
@@ -20,5 +36,5 @@ if __name__ == "__main__":
                  'y_inferior_pixel', 'x_superior_cm', 'y_superior_cm', 'x_inferior_cm',
                  'y_inferior_cm', 'lvot_diameter_cm', 'SCparams'])
 
-    iterate_folders(input_dir, output_imgs_dir, writer)
+    iterate_folders(input_dir, output_imgs_dir, output_masks_dir, height, width, with_gaussian, x_warp, y_warp, writer)
     keyfile_log.close()
